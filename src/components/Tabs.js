95e5c67938373d9d4f611900a5d3d68a24d7bece
kulanah <devonthebutler@gmail.com>
2018-04-30 13:56:40 -0700

    fixed tabs such that you can scroll through them properly

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -10,25 +10,33 @@ class Tabs extends React.Component{
 
     this.state = {
       startTab: 0,
-      tabs: ['Setup', 'Search', 'Tune', 'TEM', 'Feg Register', 'EFTEM', 'Dark Field']
+      tabs: ['Setup', 'Search', 'Tune', 'TEM', 'Feg Register', 'EFTEM', 'Dark Field'],
+      shiftedTabs: []
     };
 
 
     this.incrementArray = this.incrementArray.bind(this);
     this.decrementArray = this.decrementArray.bind(this);
   }
+
   decrementArray(){
-    if (this.state.startTab > 0){
+    if (this.state.shiftedTabs.length > 0){
+      let newtabs = this.state.tabs;
+      newtabs.unshift(this.state.shiftedTabs[0]);
       this.setState({
-        startTab: this.state.startTab - 1
+        tabs: newtabs,
+        shiftedTabs: this.state.shiftedTabs.slice(1)
       });
     }
   }
 
   incrementArray(){
-    if (this.state.startTab < this.state.tabs.length - 2){
+    if (this.state.startTab < this.state.tabs.length - 1){
+      let shifted = this.state.shiftedTabs;
+      shifted.unshift(this.state.tabs[0]);
       this.setState({
-        startTab: this.state.startTab + 1
+        shiftedTabs: shifted,
+        tabs: this.state.tabs.slice(1)
       });
     }
   }
@@ -38,9 +46,18 @@ class Tabs extends React.Component{
       <div className='tabsdiv'>
         <div className='titleBar'>Workset</div>
         <div className='tabs'>
-          <div className='tabsheader'>{this.state.tabs[this.state.startTab]}</div>
+          {this.state.tabs.map(tab =>{
+            return(
+              <div className='tabsheader'>{tab}</div>
+            );
+          })}
+          {/* <div className='tabsheader'>{this.state.tabs[this.state.startTab]}</div>
           <div className='tabsheader'>{this.state.tabs[this.state.startTab + 1]}</div>
           <div className='tabsheader'>{this.state.tabs[this.state.startTab + 2]}</div>
+          <div className='tabsheader'>{this.state.tabs[this.state.startTab + 3]}</div>
+          <div className='tabsheader'>{this.state.tabs[this.state.startTab + 4]}</div>
+          <div className='tabsheader'>{this.state.tabs[this.state.startTab + 5]}</div>
+          <div className='tabsheader'>{this.state.tabs[this.state.startTab + 6]}</div> */}
         </div>
         <span className='scrollarrows'>
           <span className='arrowspan' onClick={this.decrementArray}><img className='arrowimage' src={leftarrow} alt='Left arrow for tabs scrolling'/></span>
