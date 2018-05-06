388d941ffbcef88b60feefbe5ab0f96d02fc9f65
kulanah <devonthebutler@gmail.com>
2018-05-05 18:06:16 -0700

    switchd to named imports throughout app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
-import Tabs from './components/Tabs';
+import { Tabs } from './components/Tabs';
+import { LeftColumnWindows } from './components/LeftColumnWindows';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
         <Tabs />
+        <LeftColumnWindows />
       </div>
     );
   }
 }
 
-export default App;
+export {App};
