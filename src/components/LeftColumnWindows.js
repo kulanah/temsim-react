92a5a625875260eee104a8fbe80afa725b73e973
kulanah <devonthebutler@gmail.com>
2018-07-24 13:58:35 -0700

    updated leftcol to make more sense

--- src/components/LeftColumnWindows.js
+++ src/components/LeftColumnWindows.js
@@ -1,17 +1,4 @@
 import React from 'react';
-import beamsettings from '../img/beamsettings.png';
-import darkfield from '../img/darkfield.png';
-import detector from '../img/detector.png';
-import directalignments from '../img/directalignments.png';
-import eftemstigmator from '../img/eftemstigmator.png';
-import fegcontrol from '../img/fegcontrol.png';
-import fegregister from '../img/fegregister.png';
-import hightension from '../img/hightension.png';
-import infopanel from '../img/infopanel.png';
-import peels from '../img/peels.png';
-import stigmator from '../img/stigmator.png';
-import vacuummenu from '../img/vacuummenu.png';
-import stage from '../img/stage.png';
 import '../windows.css';
 
 import { VacuumWindow } from './VacuumWindow';
@@ -24,14 +11,6 @@ class LeftColumnWindows extends React.Component{
     super();
 
     this.state = {
-      images:  [{
-        vacuum: vacuummenu,
-        hightension: hightension,
-        fegcontrol: fegcontrol,
-      }, {
-        stage: stage,
-        beamsettings: beamsettings
-      }],
       openTab: 0
     };
   }
