91e98a8799f8b81f28c8b04244e3fa2f6df992e5
kulanah <devonthebutler@gmail.com>
2018-07-24 13:53:40 -0700

    created start menu class and css

---
+++ src/components/StartMenu.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './css/StartMenu.css';
+import '../windows.css';
+
+import startIcon from '../img/windowsStartImage.png';
+import icon from '../img/icon.png';
+import icon2 from '../img/icon2.png';
+
+class StartMenu extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      openWindows: [
+        {
+          'name': 'This is the first itme',
+          'icon': icon
+        }, {
+          'name': 'This is the second item',
+          'icon': icon2
+        }
+      ]
+    };
+  }
+
+  render(){
+    return(
+      <div id='startMenuDiv'>
+        <div id='startButton' class='taskBarButton'>
+          <span><img className='taskBarIcon' src={startIcon} alt=''/></span>
+          <span className='windowsText bold'> Start</span>
+        </div>
+        <span id='startMenuSeparator'>  </span>
+        {this.state.openWindows.map((item) => {
+          return(
+            <div className='taskBarButton windowsTask'>
+              <img className='taskBarIcon' src={item.icon} alt=''/>
+              <span className='windowsText'>
+                {item.name}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export { StartMenu };
