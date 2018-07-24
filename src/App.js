c9828a77202ec57444c155c93e01235ef63bd0c1
kulanah <devonthebutler@gmail.com>
2018-07-24 13:53:51 -0700

    integrated start menu into app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 
 import { LeftColumn } from './components/LeftColumn';
+import { StartMenu } from './components/StartMenu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <LeftColumn />
+        <StartMenu />
       </div>
     );
   }
