bc6585557f26619ae82cc67f5bea89c4e33b6cbb
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-03 11:37:01 -0700

    added the contnet prop to the base window component

--- src/components/MSWindow/Window.js
+++ src/components/MSWindow/Window.js
@@ -3,12 +3,11 @@ import { FileMenuBar } from './FileMenuBar';
 import { TitleBar } from './TitleBar';
 
 const Window = (props) => {
-  // console.log(props);
   return(
     <div className='windowsWindow'>
       <TitleBar {...props.titleBar} />
       <FileMenuBar data={props.fileBar} />
-      {/* {React.createElement(props.content)} */}
+      {props.content}
     </div>
   );
 };
