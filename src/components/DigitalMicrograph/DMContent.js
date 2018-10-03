3638c2576ecb628efde3ebb84b16e18ea27dd5a8
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-03 11:36:13 -0700

    added content window component

---
+++ src/components/DigitalMicrograph/DMContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './DigitalMicrograph.css';
+
+class DMContent extends React.Component{
+  render(){
+    return(
+      <div className='DMContentWindow'>TESTDIV</div> 
+    );
+  }
+}
+
+export { DMContent };
