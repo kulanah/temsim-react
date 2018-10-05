d57f800bdcb8b559e9f25c587753ae611983aee0
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-05 12:44:28 -0700

    Created basic outline for the dropdown react component

---
+++ src/components/MSWindow/Dropdown.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'components/css/Dropdown.css';
+import ExpandArrow from 'img/expandDropdown.svg';
+
+const Dropdown = (props) => {
+  let id = '';
+  if (props.id){
+    id = props.id;
+  }
+
+  let displayArrow = function(){
+    return(
+      <span>
+        <span className='DropdownExpandArrowOuter'>
+          <span className='DropdownExpandArrow'>
+            <img src={ExpandArrow} className='DropdownArrowImage' alt=''/>
+          </span>
+        </span>
+      </span>
+    )
+  }
+
+  return (
+    <div className='Dropdown' id={id} >
+      <span>
+        Search
+      </span>
+      {displayArrow()}
+    </div>
+  );
+};
+
+export { Dropdown }
