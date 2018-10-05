9f8e57662444e784b92ebaf8285f6618cadec587
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-05 13:45:12 -0700

    added first two rows of content to the camera box

--- src/components/DigitalMicrograph/DMCameraWindow.js
+++ src/components/DigitalMicrograph/DMCameraWindow.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { MiniTitleBar } from './MiniTitleBar';
+import { Dropdown } from 'components/MSWindow/Dropdown';
+
+import DMExpand from 'img/expandDigitalMicrograph.svg';
+import './DigitalMicrograph.css';
 
 class DMCameraWindow extends React.Component{
   render(){
     return(
       <div className='DigitalMicrographCamera'>
         <MiniTitleBar/>
-        test
+        <div className='DMContentGroup'>
+          <div className='CenterLeftContent'>
+            <span className='CameraExpand'>
+              <img src={DMExpand} alt='' />
+            </span>
+            <span>
+              Camera View
+            </span>
+          </div>
+        </div>
+        <div className='DMContentGroup'>
+          <div className='CenterLeftContent'>
+            <span>
+              Setup: 
+            </span>
+            <Dropdown id='cameraSetupDropdown'/>
+          </div>
+        </div>
       </div>
     );
   }
