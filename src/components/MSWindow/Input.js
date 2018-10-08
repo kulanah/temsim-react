564e70df706fbbbf9007a1406f8c9f45d825eefb
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-08 11:42:17 -0700

    created input component

---
+++ src/components/MSWindow/Input.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Input extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {value: 0.1};
+    this.changeValue = this.changeValue.bind(this);
+  }
+
+  changeValue(event){
+    this.setState({value: event.target.value});
+  }
+
+  getValue(){
+    return this.state.value;
+  }
+
+  render(){
+    return(
+      <div className='CenterLeftContent'>
+        <span>{this.props.label}</span>
+
+        <span><input onChange={this.changeValue} className='windowsInput removeSpinner' type='number' value={this.state.value}></input></span>
+      </div>
+    );
+  }
+}
+
+export { Input };
