ad832b0fc6fc3977930beb19c5bc7591ce502d61
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-19 14:16:29 -0700

    added skeleton of the eventual status bar in the tem ui

---
+++ src/components/TEMUI/TEMUIStatusBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'components/css/TEMUIStatusBar.css';
+
+class TEMIUIStatusBar extends React.Component{
+  render(){
+    return(
+      <div className='TEMUIStatusBar'> TEST </div>
+    );
+  }
+}
+
+export { TEMIUIStatusBar };
