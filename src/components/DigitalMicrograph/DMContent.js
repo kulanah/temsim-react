9429784ad95e116103d0db7186b5ccfcae8581a5
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-19 09:48:01 -0700

    updated styling for the dm content and added bottom window content

--- src/components/DigitalMicrograph/DMContent.js
+++ src/components/DigitalMicrograph/DMContent.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './DigitalMicrograph.css';
 import { DMCameraWindow } from './DMCameraWindow';
+import { DMBottomWindow } from './DMBottomWindow';
 
 class DMContent extends React.Component{
   render(){
     return(
-      <div className='DMContentWindow'>
-        <DMCameraWindow />
+      <div className='DMContentBackground'>
+        <div id='DMLeftColumn'>
+          <DMCameraWindow />
+          <DMBottomWindow />
+        </div>
       </div> 
     );
   }
