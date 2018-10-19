1af314a8069778cfccfd66a4f94dc41c279636db
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-19 11:13:49 -0700

    added code to display empty options dropdown

--- src/components/MSWindow/Dropdown.js
+++ src/components/MSWindow/Dropdown.js
@@ -2,30 +2,80 @@ import React from 'react';
 import 'components/css/Dropdown.css';
 import ExpandArrow from 'img/expandDropdown.svg';
 
-const Dropdown = (props) => {
-  let className = 'Dropdown';
-  if (props.className){
-    className = props.className + ' Dropdown';
+class Dropdown extends React.Component{
+  constructor(props){
+    super(props);
+
+    
+    if (this.props.className){
+      this.state = {
+        className: this.props.className + ' Dropdown',
+        selected: false,
+      };
+    } else{
+      this.state = {
+        className: 'Dropdown',
+        selected: false,
+      };
+    }
+
+    this.expandDropdown = this.expandDropdown.bind(this);
+    this.displayDropdown = this.displayDropdown.bind(this);
+    this.displayArrow = this.displayArrow.bind(this);
+    this.render = this.render.bind(this);
   }
 
-  let displayArrow = function(){
+  expandDropdown(){
+    this.setState({selected: !this.state.selected});
+    console.log('clicked expand');
+  }
+
+  displayDropdown(){
+    if (this.state.selected){
+
+      if (this.props.options){
+        return this.displayOptions();
+      } else {
+        return this.displayEmpty();
+      }
+
+    } else {
+      return null;
+    }
+  }
+
+  displayOptions(){
+
+  }
+
+  displayEmpty(){
+    return <div className='EmptyOptions' />;
+  }
+
+
+  displayArrow(){
     return(
       <span>
-        <span className='DropdownExpandArrow'>
+        <span className='DropdownExpandArrow' onClick={this.expandDropdown}>
           <img src={ExpandArrow} className='DropdownArrowImage' alt=''/>
         </span>
       </span>
     );
   };
 
-  return (
-    <div className={className} > 
-      <span>
-        Search
-      </span>
-      {displayArrow()}
-    </div>
-  );
+  render(){
+    return (
+      <div className={this.state.className} > 
+        <div className='DropdownDisplay'>
+          <span>
+            Search
+          </span>
+          {this.displayArrow()}
+        </div>
+        {this.displayDropdown()}
+      </div>
+    );
+  }
 };
 
 export { Dropdown };
