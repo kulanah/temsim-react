3c0f34b49efb1c54679bc5b69179216e26c12bb9
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-22 11:15:09 -0700

    Added status bar to the project

--- src/components/TEMUI/TEMUIContent.js
+++ src/components/TEMUI/TEMUIContent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { DMWindow } from 'components/DigitalMicrograph/DMWindow';
+import { TEMUIStatusBar } from 'components/TEMUI/TEMUIStatusBar';
 
 class TEMUIContent extends React.Component{
   render(){
     return (
       <div id='TEMUIContent'>
         <DMWindow />
+        <TEMUIStatusBar />
       </div>
     );
   } 
