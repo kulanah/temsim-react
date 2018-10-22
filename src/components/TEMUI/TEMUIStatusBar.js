3a4097881d1d4bdab401ff6948b960373cecf2df
Devon Butler <dbutler@psu.ds.pdx.edu>
2018-10-22 13:43:16 -0700

    added icons and modified style of bottom status bar

--- src/components/TEMUI/TEMUIStatusBar.js
+++ src/components/TEMUI/TEMUIStatusBar.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import blueSquare from 'img/bluesquare.svg';
+import blueUnderline from 'img/blueunderline.svg';
+import closeButton from 'img/closeicon.svg';
+import expandButtonUp from 'img/expandDropdownUp.svg';
 
 import 'components/TEMUI/TEMUIStatusBar.css';
+
+
+import { Dropdown } from 'components/MSWindow/Dropdown';
 
 class TEMUIStatusBar extends React.Component{
   render(){
     return(
-      <div className='TEMUIStatusBar StatusGrow3'>
+      <div className='TEMUIStatusBar'>
+        <div className='StatusBarHeader'>
+          <span id='StatusExpandUp'>
+            <span>
+             
+            </span>
+            <span>
+              <button className='windowsButton'><img id='StatusBarDropDownUp' src={expandButtonUp} alt='' /></button>
+            </span>
+          </span>
+          <span id='TecnaiHeader'>Tecnai G²</span>
+          <span id='StatusHeaderDropdown'>
+            <button className='windowsButton StatusBarDirectAlignButton'><img src={blueSquare} alt='' /></button>
+            <button className='windowsButton StatusBarDirectAlignButton'><img src={blueUnderline} alt='' /></button>
+            <span>
+              <Dropdown className='StatusDADropdown' />
+            </span>
+            <button className='windowsButton StatusBarDirectAlignButton'><img id='StatusDAClose' src={closeButton} alt='' /></button>
+          </span>
+        </div>
+
         <div className='StatusBarInner'>
           <span>
             Obj Lens:
