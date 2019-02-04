7802029f79da3567a799847f1f8a255e34f58696
Devon Butler <dbutler@psu.ds.pdx.edu>
2019-02-04 14:05:41 -0800

    minor refactoring

--- src/components/MSWindow/FileMenuBar.js
+++ src/components/MSWindow/FileMenuBar.js
@@ -15,20 +15,19 @@ class FileMenuBar extends React.Component{
     this.deselectMenu = this.deselectMenu.bind(this);
     this.createFileButtons = this.createFileButtons.bind(this);
     this.hoverButton = this.hoverButton.bind(this);
-    this.ClickOffDropDown= this.ClickOffDropDown.bind(this);
+    this.clickOffDropDown= this.clickOffDropDown.bind(this);
   }
 
   selectButton(title){
     this.setState({active: title});
 
-    global.document.addEventListener('click', this.ClickOffDropDown, false);
+    global.document.addEventListener('click', this.clickOffDropDown, false);
   }
 
-  ClickOffDropDown(event){
-    console.log(event.target);
+  clickOffDropDown(event){
     if (event.target.className !== 'FileMenuDropDown'){
       this.setState({active: null});
-      global.document.removeEventListener('click', this.ClickOffDropDown);
+      global.document.removeEventListener('click', this.clickOffDropDown);
     }
   }
 
