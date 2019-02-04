144834ecdcac10b5e2f278ecc2c7d4cee02a428d
Devon Butler <dbutler@psu.ds.pdx.edu>
2019-02-04 12:26:28 -0800

    created framework for popup rows

--- src/components/MSWindow/Popup.js
+++ src/components/MSWindow/Popup.js
@@ -1,12 +1,56 @@
 import React from 'react';
 
 import 'windows.css';
+import 'components/css/FileBarDropDown.css';
 
+let displayDropdownContent = function(rows){
+  let dropDown = rows.map(row => {
+    if (row.type === 'hotkey'){
+      return displayHotkeyRow(row);
+    } else if (row.type === 'single'){
+      return displaySingleRow(row);
+    } else {
+      return 'blank row';
+    }
+  });
+
+  return dropDown;
+};
+
+let displaySingleRow = function(row){
+  let returnVal = (
+    <span className='DropDownRow'>
+      <span className='OneItemRow'>
+        {row.title}
+      </span>
+    </span>
+  );
+
+  return returnVal;
+};
+
+let displayHotkeyRow = function(row){
+  let returnVal = (
+    <span className='DropDownRow'>
+      <span className='TwoItemRow'>
+        {row.title}
+        {row.hotkey}
+      </span>
+    </span>
+  );
+
+  return returnVal;
+};
 
 const Popup = (props) => {
   let cssVal = 'top: ' + props.clientY + '; left: ' + props.clientX + ';';
   return (
-    <div className='testDiv' css={cssVal}> {props.header} </div>
+    <div className='FileMenuDropDown' css={cssVal}> 
+      <span className='OneItemRow'>
+        {displayDropdownContent(props.buttons)}
+      </span>
+    
+    </div>
   );
 };
 
